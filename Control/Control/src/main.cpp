ec61aa996c0c6ef30b7acd528f45670ee71f9dc8
Lasxr <laserp.coding@gmail.com>
2025-08-06 23:47:53 +0700

    LoRa Connected

--- Control/Control/src/main.cpp
+++ Control/Control/src/main.cpp
@@ -1,33 +1,88 @@
 #include<Arduino.h>
 #include <SPI.h>
 #include <LoRa.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+
+#define OLED_RESET    -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 const int Xpin = 35;
 const int Ypin = 33;
 const int SWpin = 16;
 
+int DeadZone = 300;
+int Center = 2800;
+
+int smoothX = 0;
+int smoothY = 0;
+
+int On_Board = 2;
 void setup() {
 
   pinMode(SWpin, INPUT_PULLUP);
+  pinMode(On_Board, OUTPUT);
 
   Serial.begin(115200);
 
   LoRa.setPins(5, 14, 26);  // NSS, RST, DIO0
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true);
+  }
 
   if (!LoRa.begin(433E6)) {
     Serial.println("LoRa init failed!");
     while (1);
   }
   //LoRa.setTxPower(10);
-
+  
   Serial.println("LoRa Receiver Ready");
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Start Program Control");
+  display.println(" ");
+  display.println("        Ready!");
+  display.display();
 
 }
 
-void loop() {
-  int ValX = analogRead(Xpin);
-  int ValY = analogRead(Ypin);
+void JoySent(int Speed){
+
+  int RawX = analogRead(Xpin);
+  int RawY = analogRead(Ypin);
   int SWval = digitalRead(SWpin);
+
+
+  int joyX = RawX - Center;
+  int joyY = RawY - Center;
+
+
+  if (abs(joyX) < DeadZone) joyX = 0;
+  if (abs(joyY) < DeadZone) joyY = 0;
+  
+  joyX = constrain(joyX, -1295, 1295);
+  joyY = constrain(joyY, -1295, 1295);
+
+  smoothX = 0.3 * smoothX + 0.2 * joyX;
+  smoothY = 0.3 * smoothY + 0.2 * joyY;
+  
+  int ValX = map(smoothX, -323, 323, -Speed, Speed);
+  int ValY = map(smoothY, -323, 323, -Speed, Speed);
+  
+  ValX = constrain(ValX, -Speed , Speed);
+  ValY = constrain(ValY, -Speed , Speed);
+
+  ValX = -ValX;
+  ValY = -ValY;
 
   Serial.print("Val X: ");
   Serial.print(ValX);
@@ -45,6 +100,30 @@ void loop() {
   LoRa.print(SWval == LOW ? 1 : 0);
   LoRa.endPacket();
 
+  display.fillRect(0, 40, 128, 24, SSD1306_BLACK); // ลบเฉพาะส่วนแสดงค่าเก่า
+  display.setCursor(0, 40);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.print("      ");
+  display.print(ValX);
+  display.print(" | ");
+  display.print(ValY);
+  display.print(" | ");
+  display.print(SWval == LOW ? 1 : 0);
+  display.display();
 
+  if (ValX == 125 || ValX == -125 || ValY == 125 || ValY == -125) {
+  display.setCursor(0, 54);
+  display.print("      Max Speed !");
+  display.display();
+  digitalWrite(On_Board, 1);
+}else{
+  digitalWrite(On_Board, 0);
+}
+}
+
+
+void loop() {
+  JoySent(125);   //ถ้ามึงออกเกิน 200 Driver มึงกลับสวรรค์แน่
   delay(50);
 }
--- New Driver/New_Driver/src/main.cpp
+++ New Driver/New_Driver/src/main.cpp
@@ -2,20 +2,46 @@
 #include <SPI.h> 
 #include <LoRa.h>
 
+int On_Board = 2;
+
+unsigned long lastReceiveTime = 0;
+unsigned long timeoutDuration = 5000;
+
+int xVal, yVal, swVal;
 
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);
+
+  pinMode(On_Board, OUTPUT);
 
   LoRa.setPins(5, 14, 26);    // NSS, RST, DIO0
   if (!LoRa.begin(433E6)) {
     Serial.println("LoRa init failed!");
+    digitalWrite(On_Board, 0);
     while (1);
   }
   //LoRa.setTxPower(10);
-
+  digitalWrite(On_Board, 1);
   Serial.println("LoRa Receiver Ready");
 
 
+}
+
+void parseData(String data) {
+  int xIndex = data.indexOf("X:") + 2;
+  int yIndex = data.indexOf("Y:") + 2;
+  int swIndex = data.indexOf("SW:") + 3;
+
+  int comma1 = data.indexOf(',');
+  int comma2 = data.lastIndexOf(',');
+
+  String xStr = data.substring(xIndex, comma1);
+  String yStr = data.substring(yIndex, comma2);
+  String swStr = data.substring(swIndex);
+
+  xVal = xStr.toInt();
+  yVal = yStr.toInt();
+  swVal = swStr.toInt();
 }
 
 void loop() {
@@ -27,6 +53,18 @@ void loop() {
     }
     Serial.print("Recived : ");
     Serial.println(msg);
+
+    parseData(msg);
+    Serial.print("X: "); Serial.println(xVal);
+    Serial.print("Y: "); Serial.println(yVal);
+    Serial.print("SW: "); Serial.println(swVal);
+
+    lastReceiveTime = millis();
   }
-  delay(20);
+  if (millis() - lastReceiveTime > timeoutDuration) {
+  xVal = 0;
+  yVal = 0;
+  swVal = 0;
+  digitalWrite(On_Board, LOW);
+}
 }
